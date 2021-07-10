82e494d7caa15f486c4d764e8726efbd123a6a09
jafapebe <ing.jairoperpinan@gmail.com>
2021-07-10 07:41:37 -0500

    suma de variables

--- src/empresa/Ejemplo.java
+++ src/empresa/Ejemplo.java
@@ -5,6 +5,11 @@ public class Ejemplo {
         Cliente uno = new Cliente("Jairo", 28, 316966798L);
 
         System.out.println("hola");
+        int r=8:
+        int s=5;
+        int su=r+s;
+
+        System.out.println(su);
 
         uno.SetEdad(40);
 
